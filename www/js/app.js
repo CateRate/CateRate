187a0195d168019953561468ea391af027d127e1
boris <korneev.boris@gmail.com>
2014-04-30 19:26:00 +0300

    Changed ngRoute to ui-router

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('starter', ['ionic', 'starter.controllers', 'firebase', 'ngRoute'])
+var app = angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -15,61 +15,35 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'firebase',
   });
 });
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/login', {templateUrl: 'CateRate/www/templates/_login.html', controller: 'LoginCtrl'})
-                  .when('/organizations', {templateUrl: 'CateRate/www/templates/_organizations.html', controller: 'OrganizationsCtrl'});
-    $routeProvider.otherwise({redirectTo: '/login'});
+//app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+//    $routeProvider.when('/login', {templateUrl: 'templates/_login.html', controller: 'LoginCtrl'})
+//                  .when('/organizations', {templateUrl: 'templates/_organizations.html', controller: 'OrganizationsCtrl'});
+//    $routeProvider.otherwise({redirectTo: '/login'});
+//
+//    $locationProvider.html5Mode(true);
+//}]);
 
-    $locationProvider.html5Mode(true);
-}]);
+app.config(function($stateProvider, $urlRouterProvider) {
 
-//.config(function($stateProvider, $urlRouterProvider) {
-//  $stateProvider
-//
-//    .state('app', {
-//      url: "/app",
-//      abstract: true,
-//      templateUrl: "templates/menu.html",
-//      controller: 'AppCtrl'
-//    })
-//
-//    .state('app.search', {
-//      url: "/search",
-//      views: {
-//        'menuContent' :{
-//          templateUrl: "templates/search.html"
-//        }
-//      }
-//    })
-//
-//    .state('app.browse', {
-//      url: "/browse",
-//      views: {
-//        'menuContent' :{
-//          templateUrl: "templates/browse.html"
-//        }
-//      }
-//    })
-//    .state('app.playlists', {
-//      url: "/playlists",
-//      views: {
-//        'menuContent' :{
-//          templateUrl: "templates/playlists.html",
-//          controller: 'PlaylistsCtrl'
-//        }
-//      }
-//    })
-//
-//    .state('app.single', {
-//      url: "/playlists/:playlistId",
-//      views: {
-//        'menuContent' :{
-//          templateUrl: "templates/playlist.html",
-//          controller: 'PlaylistCtrl'
-//        }
-//      }
-//    });
-//  // if none of the above states are matched, use this as the fallback
-//  $urlRouterProvider.otherwise('/app/playlists');
-//});
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
+        // setup an abstract state for the tabs directive
+        .state('login', {
+            url: "/login",
+            templateUrl: "templates/_login.html",
+            controller: "LoginCtrl"
+        })
+        .state('organizations', {
+            url: "/organizations",
+            templateUrl: "templates/_organizations.html",
+            controller: "OrganizationsCtrl"
+        })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/login');
+
+});
