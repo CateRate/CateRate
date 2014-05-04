6ab35bd8b092d71fb1f7f1a7b4ef3deb079d82f2
boris <korneev.boris@gmail.com>
2014-05-04 12:46:51 +0300

    Fixed login button

---
+++ platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/boris/Development/CateRate/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
+/Users/boris/Development/CateRate/platforms/android/ant-build/HelloCordova.ap_ \
+/Users/boris/Development/CateRate/platforms/android/ant-build/classes.dex \
+/Users/boris/Development/CateRate/platforms/android/CordovaLib/ant-build/classes.jar \
